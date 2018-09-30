dd69be8967de326b63c3be1f09ad01d910b57148
chenlei <249366869@qq.com>
2018-09-30 14:07:27 +0800

    第二题应该算是结束了，但是不想写那个shabi文档錒！！！怎么那么麻烦！！！

--- 两个有序链表序列的交集/两个有序链表序列的交集/src.cpp
+++ 两个有序链表序列的交集/两个有序链表序列的交集/src.cpp
@@ -7,27 +7,32 @@ int main()
 {
 	list<int>s1, s2, ans;
 	int input;
-	cin >> input;
-	while (input != -1);
+	//读取两链表值
+	do
 	{
+		cin >> input;
 		s1.push_back(input);
-		cin >> input;
-	}
-	cin >> input;
-	while (input != -1);
+	} while (input != -1);
+	
+	do
 	{
-		s2.push_back(input);
 		cin >> input;
-	}
+		s2.push_back(input);
+	} while (input != -1);
+
 	auto it1 = s1.begin(), it2 = s2.begin();
-	while (it1 != s1.end() || it2 != s2.end())
+
+	while (*it1 != -1 && *it2 != -1)
 	{
 		if (*it1 == *it2)
+			//两结点值相等，将结果存入ans，同时递增两个迭代器
 		{
 			ans.push_back(*it1);
 			it1++; it2++;
 			continue;
 		}
+
+		//两结点值不相等，递增值小的迭代器
 		else if (*it1 < *it2)
 		{
 			it1++;
@@ -41,15 +46,16 @@ int main()
 	}
 
 	if (ans.empty())
+		//没有相等的元素
 		cout << "NULL";
 	else
 	{
 		for (auto num : ans)
 		{
-			cout << num;
+			cout << num << " ";
 		}
 	}
 
-	system("pausa");
+	system("pause");
 	return 0;
 }
