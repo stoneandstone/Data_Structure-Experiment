0ff35179c85047c5f203a57a5cdf9d09268f8df3
chenlei <249366869@qq.com>
2018-10-11 22:57:48 +0800

    嘤嘤嘤

--- 勇闯迷宫游戏/勇闯迷宫游戏/src.cpp
+++ 勇闯迷宫游戏/勇闯迷宫游戏/src.cpp
@@ -1,79 +1,145 @@
 #include <iostream>
-#include <stack>
-#include <string>
-#include <memory>
+#include <vector>
 #include <assert.h>
 
 using namespace std;
 
-class Offset
-{
-public:
-	int x, y;
-	string dir;
-};
+int** maze;
+int row, col;
+
 
 class Node
 {
 public:
-	int x;
-	int y;
-	int dir;
-};
+	int row;
+	int col;
 
-void createMaze(int** &maze, int** &mark, int&row, int& col)
+	friend istream& operator>>(istream& input, Node& obj)
+	{
+		input >> obj.row >> obj.col;
+		return input;
+	}
+	bool operator==(const Node& obj)
+	{
+		return (row==obj.row&&col==obj.col);
+	}
+};  
+
+Node beginNode, endNode;
+Node turn[8] = { {0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1},{-1,0},{-1,1} };
+vector<Node>path;
+
+void createMaze()
 {
 	cout << "请输入迷宫的行数和列数" << endl;
 	cin >> row >> col;
 	maze = new int*[row];
-	assert(maze == nullptr);
-	mark = new int*[row];
-	assert(mark == nullptr);
+	assert(maze);
 	for (int i = 0; i < row; ++i)
 	{
 		maze[i] = new int[col];
-		assert(maze[i] == nullptr);
-		mark[i] = new int[col];
-		assert(mark[i] == nullptr);
+		assert(maze[i]);
 	}
 	for (int i1 = 0; i1 < row; ++i1)
 	{
-		cout << "请输入第" << i1 + 1 << "行迷宫 共"<< col<<"个数字（0代表路,1代表围墙）"<< endl;
+		cout << "请输入第" << i1  << "行迷宫 共"<< col<<"个数字（0代表路,1代表围墙）"<< endl;
 		for (int i2 = 0; i2 < col; ++i2)
 		{
 			cin >> maze[i1][i2];
 		}
 	}
 	cout << "所输入的迷宫为" << endl;
+	cout << "        ";
+	for (int i = 0; i < col; ++i)
+	{
+		cout << "第" << i << "列 ";
+	}
+	cout << endl;
 	for (int i1 = 0; i1 < row; ++i1)
 	{
+		cout << "第" << i1 << "行     ";
 		for (int i2 = 0; i2 < col; ++i2)
 		{
-			cout << maze[i1][i2] << " ";
+			cout << maze[i1][i2] << "     ";
 		}
 		cout << endl;
 	}
+
+
+	do
+	{
+		cout << "请输入迷宫的起点和终点 先输入行再输入列以空格分开" << endl;
+		cin >> beginNode >> endNode;
+		if (endNode.row<0 || endNode.col<0 || endNode.row>row || endNode.col>col)
+			continue;
+		if (beginNode.row<0 || beginNode.col<0 || beginNode.row>row || beginNode.col>col)
+			continue;
+
+	} while (maze[beginNode.row][beginNode.col] && maze[endNode.row][endNode.col]);
+	int a;
 }
 
-void seekPath(int beginx, int beginy, int endx, int endy, stack<Node>&path)
+bool findNode(const Node& obj)
 {
-	 
+	for (auto n : path)
+	{
+		if (n == obj)
+			return true;
+	}
+	return false;
 }
+
+void outputPath()
+{
+	static int count = 1;
+	cout << "第" << count << "条路如下" << endl;
+	for (auto node : path)
+	{
+		cout << "(" << node.row << "," << node.col << ")";
+		if (!(node == *path.rbegin()))
+		{
+			cout << "----->";
+		}
+	}
+	count++;
+}
+
+void seekPath(Node& past)
+{
+	static bool isFind = false;
+	if (isFind)
+		return;
+	if (past == endNode)
+	{
+		outputPath();
+		path.erase(path.end() - 1);
+		return;
+	 }
+	for (int i = 0; i < 8; ++i)
+	{
+		Node cur;
+		cur.row = past.row + turn[i].row;
+		cur.col = past.col + turn[i].col;
+		if (cur.row >= 0 && cur.row < row&&cur.col >=0 && cur.col < col)
+		{
+			if (maze[cur.row][cur.col] == 0&&!findNode(cur))
+			{
+				path.push_back(cur);
+				seekPath(cur);
+			}
+		}
+	}
+	path.erase(path.end() - 1);
+}
+
+
 
 int main()
 {
-	int** maze, **mark;
-	int row, col;
-	createMaze(maze, mark, row, col);
-	Offset move[8] = { {-1,0,"N"} ,{-1,1,"NE"},{0,1,"E"},{1,1,"SE"},{1,0,"S"},{1,-1,"SW"},{0,-1,"W"},{-1,-1,"NW"} };
-
-	int beginx, beginy, endx, endy;
-	do
-	{
-		cin >> beginx >> beginy >> endx >> endy;
-	} while (!maze[beginx][beginy] && !maze[endx][endy]);
-
-	stack<Node>path;
-
-	return 0;
+	createMaze();
+	path.push_back(beginNode);
+	seekPath(beginNode);
+	
+	system("pause");
+	return 0; 
 }
