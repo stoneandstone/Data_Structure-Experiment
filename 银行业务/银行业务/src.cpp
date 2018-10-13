2e7442df95a4457ffc0f49bd9715a351ccfe158c
chenlei <249366869@qq.com>
2018-10-13 20:12:17 +0800

    第四题做了啊，文档又没做啊

---
+++ 银行业务/银行业务/src.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	int num, curnum;
+	cin >> num;
+	queue<int>windowa, windowb;
+	for (int i = 0; i < num; ++i)
+	{
+		cin >> curnum;
+		if (curnum % 2)
+			//奇数号去A窗口，偶数号去B窗口
+		{
+			windowa.push(curnum);
+		}
+		else
+		{
+			windowb.push(curnum);	
+		}
+	}
+	while (!windowa.empty() || !windowb.empty())
+	{
+		if (!windowa.empty())
+		{
+			for (int i = 1; i <= 2; ++i)
+			{
+				curnum = windowa.front();
+				windowa.pop();
+				cout << curnum << " ";
+				if (windowa.empty())
+					break;
+			}
+		}
+
+		if (!windowb.empty())
+		{
+			curnum = windowb.front();
+			windowb.pop();
+			cout << curnum;
+			if (windowb.empty())
+				break;
+			cout << " ";
+		}
+	}
+
+	while (!windowa.empty())
+	{
+		curnum = windowa.front();
+		windowa.pop();
+		cout << " " << curnum;
+	}
+
+	system("pause");
+	return 0;
+}
