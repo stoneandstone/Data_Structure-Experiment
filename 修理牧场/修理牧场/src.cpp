69d1cf080aea93a6734c3cc1e8bc9a5a61351a27
chenlei <249366869@qq.com>
2018-10-21 21:10:57 +0800

    第七题修理牧场，暂时不想写注释啊啊啊啊啊啊

---
+++ 修理牧场/修理牧场/src.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int num;
+	cout << "请输入木头的数量" << endl;
+	cin >> num;
+	vector<int>ivec(num);
+	for (auto& r : ivec)
+	{
+		cin >> r;
+	}
+
+	unsigned sum = 0;
+	while (ivec.size() != 1)
+	{
+		sort(ivec.begin(), ivec.end(), [](int a, int b) {return a > b; });
+		int temp = *(ivec.end() - 1) + *(ivec.end() - 2);
+		sum += temp;
+		//ivec.erase(ivec.end()-1);
+		ivec.pop_back();
+		ivec.at(ivec.size() - 1) = temp;
+	}
+
+	cout << sum;
+
+	system("pause");
+	return 0;
+}
